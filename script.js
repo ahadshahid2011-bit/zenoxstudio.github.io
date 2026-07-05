3143a0f4a2812ce68bd65636ce79c0232dc25c1d
Abdul Ahad Khan <ahadshahid2011@gmail.com>
2026-07-05 08:05:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    function createRipple(event) {
+        const element = event.currentTarget;
+
+        const circle = document.createElement('span');
+        const diameter = Math.max(element.clientWidth, element.clientHeight);
+        const radius = diameter / 2;
+
+        circle.style.width = circle.style.height = `${diameter}px`;
+        circle.style.left = `${event.clientX - element.offsetLeft - radius}px`;
+        circle.style.top = `${event.clientY - element.offsetTop - radius}px`;
+        circle.classList.add('ripple');
+
+        const ripple = element.getElementsByClassName('ripple')[0];
+        if (ripple) {
+            ripple.remove();
+        }
+
+        element.appendChild(circle);
+    }
+
+    const serviceBoxes = document.querySelectorAll('.service-box');
+
+    serviceBoxes.forEach(box => {
+        box.addEventListener('click', createRipple);
+    });
+
+});
